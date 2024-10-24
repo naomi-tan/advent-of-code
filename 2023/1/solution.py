ceee6d2195139e3f486336be21270ae71e28a37f
naomi-tan <69682497+naomi-tan@users.noreply.github.com>
2024-10-24 10:00:09 +0100

    feat: day1 part I and II working

---
+++ 2023/1/solution.py
@@ -0,0 +1,78 @@
+# only find first and last
+# add unittest
+
+number_dict = {
+    'zero': 0,
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9
+}
+
+
+def get_data(path: str) -> str:
+    file = open(path, 'r')
+    data:str = file.read()
+    file.close()
+    return data
+
+
+def get_line_ls(text: str) -> list[str]:
+    return text.split('\n')
+
+
+def part1(lines: list[str]) -> int:
+    # split line into list of chars
+    total = 0
+    for line in lines:
+        nums = []
+        for char in line:
+            # check if char is numeric
+            if char.isnumeric():
+                nums.append(char)
+        # get first and last number, concat and convert to number
+        calibration_value = int(nums[0] + nums[-1])
+        # add to sum total
+        total += calibration_value
+    return total
+
+
+def part2(lines: list[str]) -> int:
+    # split line into list of chars
+    total = 0
+    for line in lines:
+        nums = []
+        index = 0
+        for char in line:
+            # check if char is numeric
+            if char.isnumeric():
+                nums.append(char)
+            else:
+                line_slice = line[index: index + 5]  # max string length of number word is 5 chars
+                for number in number_dict:
+                    if line_slice.find(number) == 0:
+                        nums.append(str(number_dict[number]))
+            index += 1
+        # get first and last number, concat and convert to number
+        calibration_value = int(nums[0] + nums[-1])
+        # add to sum total
+        total += calibration_value
+    return total
+
+
+def main() -> None:
+    input_data: str = get_data('input.txt')
+    lines: list[str] = get_line_ls(input_data)
+    print('Part I:')
+    print(part1(lines))
+    print('Part II:')
+    print(part2(lines))
+
+
+if __name__ == '__main__':
+    main()
---
+++ 2023/1/solution_unittest.py
@@ -0,0 +1,11 @@
+import unittest
+
+class UnitTests(unittest.TestCase):
+    def test_part1(self):
+        self.assertEqual(142 , 142)
+
+    def test_part2(self):
+        self.assertEqual(281 , 281)
+
+if __name__ == '__main__':
+    unittest.main()
