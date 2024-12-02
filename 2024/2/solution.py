65d614bdb3ca4612f375ae799a1c37a371b58085
naomi-tan <69682497+naomi-tan@users.noreply.github.com>
2024-12-02 10:42:06 +0000

    20204 day2 part1 alternative

--- 2024/2/solution.py
+++ 2024/2/solution.py
@@ -1,10 +1,11 @@
 from utils.utils import *
+import numpy as np
 
 def part1(input_data: list[str]) -> int:
     print('-----Part1-----')
     count: int = 0
     for report in input_data:
-        if check_safety(report):
+        if check_safety2(report):
             count += 1
     return count
 
@@ -40,9 +41,41 @@ def check_safety(report: str) -> bool:
 
     return safe
 
+def check_safety2(report: str) -> bool:
+    # split report into numbers
+    values: object = np.array(list(map(lambda a: int(a), report.split())))
+
+    # shift numbers 1 position
+    shift: object = np.insert(values, 0, [0])
+
+    # find difference between values
+    subtract: object = shift - np.insert(values, values.size, [0])
+    diff: object = np.delete(subtract, [0, subtract.size - 1])
+
+    inc: list[int] = list(filter(lambda a: a > 0, diff.tolist()))
+    dec: list[int] = list(filter(lambda a: a < 0, diff.tolist()))
+
+    # if all increasing or all decreasing
+    if (len(inc) == diff.size) or (len(dec) == diff.size):
+        # if 0 < absolute value < 4
+        absolute: list[int] = list(filter(lambda a: 0 < abs(a) < 4, diff.tolist()))
+        if len(absolute) == diff.size:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
 def part2(input_data: list[str]) -> int:
     print('-----Part2-----')
     ans = 0
+    # if fails, remove number it fails on and test
+    # if that fails remove other numbers
+    # if all fails, not safe
+
+    # shift all vals 1 space
+    # subtract
     return ans
 
 def main() -> None:
