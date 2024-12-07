39bedff89635f084f6f99e6ce48b91562c8224e4
naomi-tan <69682497+naomi-tan@users.noreply.github.com>
2024-12-07 18:14:17 +0000

    2024 separate part1 and 2

--- 2024/7/solution.py
+++ 2024/7/solution.py
@@ -2,30 +2,30 @@ def part1(input_data: list[str]) -> int:
     print('-----Part1-----')
     ans: int = 0
     # representing operators as a number which is then converted to base 4
-    # operators = {0: '+', 1: '-', 2: '*', 3: '/'}
+    # operators = {0: '+', 1: '*', 2: '||'}
 
     for line in input_data:
         test_val, nums = line.split(':')
         nums = nums.split()
 
         n_ops: int = (len(nums) - 1) # number of operators is 1 less than number of numbers
-        max_ops: int = pow(3, n_ops) # maximum value operators can get
+        max_ops: int = pow(2, n_ops) # maximum value operators can get
 
         for ops in range(max_ops):
-            operators = list(base10_to_base4(ops).zfill(n_ops))
+            operators = list(base10_to_basen(ops, 2).zfill(n_ops), )
             solution = calculate(nums.copy(), operators)
             if solution == int(test_val):
                 ans += solution
                 break
     return ans
 
-def base10_to_base4(ops: int) -> str:
-    quotient: int = int(ops / 3)
-    remainder: int = ops % 3
+def base10_to_basen(ops: int, base: int) -> str:
+    quotient: int = int(ops / base)
+    remainder: int = ops % base
     if quotient == 0:
         return str(remainder)
     else:
-        return base10_to_base4(quotient) + str(remainder)
+        return base10_to_basen(quotient, base) + str(remainder)
 
 def calculate(nums: list[str], operators: list[str]) -> int:
     result = nums[0]
@@ -37,7 +37,7 @@ def calculate(nums: list[str], operators: list[str]) -> int:
     return result
 
 def perform_operation(num1: str, num2: str, operator: str) -> int | float:
-    # operators = {0: '+', 1: '-', 2: '*', 3: '/'}
+    # operators = {0: '+', 1: '-', 2: '||'}
     num1 = int(num1)
     num2 = int(num2)
     match operator:
@@ -47,12 +47,23 @@ def perform_operation(num1: str, num2: str, operator: str) -> int | float:
             return num1 * num2
         case '2':
             return int(str(num1) + str(num2))
-        case '3':
-            return num1 / num2
 
 def part2(input_data: list[str]) -> int:
     print('-----Part2-----')
     ans: int = 0
+    for line in input_data:
+        test_val, nums = line.split(':')
+        nums = nums.split()
+
+        n_ops: int = (len(nums) - 1) # number of operators is 1 less than number of numbers
+        max_ops: int = pow(3, n_ops) # maximum value operators can get
+
+        for ops in range(max_ops):
+            operators = list(base10_to_basen(ops, 3).zfill(n_ops), )
+            solution = calculate(nums.copy(), operators)
+            if solution == int(test_val):
+                ans += solution
+                break
     return ans
 
 def read_lines(path: str) -> list[str]: # get_data
