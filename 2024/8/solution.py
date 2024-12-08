3946c6279d7dacb5ff175136d02724580b7a7d3a
naomi-tan <69682497+naomi-tan@users.noreply.github.com>
2024-12-08 19:43:47 +0000

    2024 day8 part1 and 2 working

--- 2024/8/solution.py
+++ 2024/8/solution.py
@@ -1,18 +1,95 @@
 from utils import utils
+import numpy as np
+from itertools import combinations
 
-def part1(input_data: list[str]) -> int:
+def part1(input_data: list[list[str]]) -> int:
     print('-----Part1-----')
     ans: int = 0
+    input_data = np.array(input_data)
+
+    # get all antenna chars
+    [antenna_loc_x, antenna_loc_y] = np.where(input_data != '.')
+
+    # box sort each char locations
+    antenna_dict: dict = {}
+    for x, y in zip(antenna_loc_x, antenna_loc_y):
+        if input_data[x, y].item() in antenna_dict:
+            antenna_dict[input_data[x, y].item()].append([x.item(), y.item()])
+        else:
+            antenna_dict[input_data[x, y].item()] = [[x.item(), y.item()]]
+
+    # get all anti-node locations for all combinations
+    anti_node_loc = []
+    for key, value in antenna_dict.items():
+        combos = combinations(value, 2)
+        for c in combos:
+            anti_node_loc.extend(get_anti_node_loc(c[0], c[1], input_data.shape))
+    ans = len(set(anti_node_loc))
     return ans
 
-def part2(input_data: list[str]) -> int:
+def get_anti_node_loc(node1: list[int], node2: list[int], shape:np.array) -> list[tuple]:
+    n1 = np.array(node1)
+    n2 = np.array(node2)
+    d = n2 - n1
+    anti_nodes = []
+    a1 = n1 - d
+    a2 = n1 + 2 * d
+    if 0 <= a1[0] < shape[0] and 0 <= a1[1] < shape[1]:
+        anti_nodes.append(tuple([a1[0].item(), a1[1].item()]))
+    if 0 <= a2[0] < shape[0] and 0 <= a2[1] < shape[1]:
+        anti_nodes.append(tuple([a2[0].item(), a2[1].item()]))
+    return anti_nodes
+
+def get_harmonic_loc(node1: list[int], node2: list[int], shape:np.array) -> list[tuple]:
+    n1 = np.array(node1)
+    n2 = np.array(node2)
+    d = n2 - n1
+    anti_nodes = []
+    a1_flag = False
+    a2_flag = False
+    i = 0
+    while not (a1_flag and a2_flag):
+        a1 = n1 - i * d
+        a2 = n1 + d + i * d
+        if 0 <= a1[0] < shape[0] and 0 <= a1[1] < shape[1]:
+            anti_nodes.append(tuple([a1[0].item(), a1[1].item()]))
+        else:
+            a1_flag = True
+        if 0 <= a2[0] < shape[0] and 0 <= a2[1] < shape[1]:
+            anti_nodes.append(tuple([a2[0].item(), a2[1].item()]))
+        else:
+            a2_flag = True
+        i += 1
+    return anti_nodes
+
+def part2(input_data: list[list[str]]) -> int:
     print('-----Part2-----')
     ans: int = 0
+    input_data = np.array(input_data)
+
+    # get all antenna chars
+    [antenna_loc_x, antenna_loc_y] = np.where(input_data != '.')
+
+    # box sort each char locations
+    antenna_dict: dict = {}
+    for x, y in zip(antenna_loc_x, antenna_loc_y):
+        if input_data[x, y].item() in antenna_dict:
+            antenna_dict[input_data[x, y].item()].append([x.item(), y.item()])
+        else:
+            antenna_dict[input_data[x, y].item()] = [[x.item(), y.item()]]
+
+    # get all anti-node locations for all combinations
+    anti_node_loc = []
+    for key, value in antenna_dict.items():
+        combos = combinations(value, 2)
+        for c in combos:
+            anti_node_loc.extend(get_harmonic_loc(c[0], c[1], input_data.shape))
+    ans = len(set(anti_node_loc))
     return ans
 
 def main() -> None:
     print('-----DayN-----')
-    input_data: list[str] = utils.read_lines('input.txt')
+    input_data: list[list[str]] = utils.read_char_arr('input.txt')
     print(part1(input_data))
     print(part2(input_data))
 
--- 2024/8/test.py
+++ 2024/8/test.py
@@ -2,9 +2,9 @@ import unittest
 from utils import utils
 import solution
 
-test_data = utils.read_lines('test.txt')
-expected_result1: int = -1
-expected_result2: int = -1
+test_data = utils.read_char_arr('test.txt')
+expected_result1: int = 14
+expected_result2: int = 34
 
 class TestSolution(unittest.TestCase):
 
