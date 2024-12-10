a45e05e2af3a86091e9b8b782af273c7680a6647
naomi-tan <69682497+naomi-tan@users.noreply.github.com>
2024-12-10 22:44:59 +0000

    2024 day10 update var names

--- 2024/10/solution.py
+++ 2024/10/solution.py
@@ -4,7 +4,17 @@ from utils import TreeUtils
 def part1(input_data: list[list[int]]) -> int:
     print('-----Part1-----')
     ans: int = 0
+    trail_heads = get_trail_heads(input_data)
+    for i in range(len(trail_heads)):
+        distinct_trail_ends = create_tree(input_data, trail_heads[i])
+        total_summits = 0
+        for end in set(distinct_trail_ends):
+            if end[0] == 9:
+                total_summits += 1
+        ans += total_summits
+    return ans
 
+def get_trail_heads(input_data: list[list[int]]) -> list[list[int]]:
     # find all 0s
     zeros = []
     row = 0
@@ -15,66 +25,39 @@ def part1(input_data: list[list[int]]) -> int:
                 zeros.append([row, col])
             col += 1
         row += 1
+    return zeros
 
-    for i in range(len(zeros)):
-        [root, all_ends] = create_tree(input_data, zeros[i])
-        total_summits = 0
-        for end in set(all_ends):
-            if end[0] == 9:
-                total_summits += 1
-        ans += total_summits
-    return ans
-
-class Data:
-    def __init__(self, value, row, col):
-        self.value = value
-        self.row = row
-        self.col = col
-
-def create_tree(input_data: list[list[int]], root_node: list[int]) -> [TreeUtils.Node|list[tuple]]:
-    # data = Data(input_data[root_node[0]][root_node[1]], root_node[0], root_node[1])
+def create_tree(input_data: list[list[int]], root_node: list[int]) -> list[tuple]:
     data = tuple([input_data[root_node[0]][root_node[1]], root_node[0], root_node[1]])
     root = TreeUtils.Node(data)
     neighbour_pos = [[-1, 0], [0, 1], [1, 0], [0, -1]]
     parent_nodes = [root]
-    all_ends = []
+    distinct_trail_ends = []
     while True:
         child_nodes = []
         for node in parent_nodes:
             neighbours = utils.get_manhattan_neighbours(input_data, [node.data[1], node.data[2]])
             for j in range(4):
                 if neighbours[j] == node.data[0] + 1:
-                    # data = Data(neighbours[j], node.data[1] + neighbour_pos[j][0], node.data[2] + neighbour_pos[j][1])
                     data = tuple([neighbours[j], node.data[1] + neighbour_pos[j][0], node.data[2] + neighbour_pos[j][1]])
                     child = TreeUtils.Node(data)
                     node.add_child(child)
                     child_nodes.append(child)
-                    all_ends.append(child.data)
+                    distinct_trail_ends.append(child.data)
         parent_nodes = child_nodes
         if len(child_nodes) == 0:
             break
-    return [root, all_ends]
-
+    return distinct_trail_ends
 
 def part2(input_data: list[list[int]]) -> int:
     print('-----Part2-----')
     ans: int = 0
+    trail_heads = get_trail_heads(input_data)
 
-    # find all 0s
-    zeros = []
-    row = 0
-    for line in input_data:
-        col = 0
-        for char in line:
-            if char == 0:
-                zeros.append([row, col])
-            col += 1
-        row += 1
-
-    for i in range(len(zeros)):
-        [root, all_ends] = create_tree(input_data, zeros[i])
+    for i in range(len(trail_heads)):
+        distinct_trail_ends = create_tree(input_data, trail_heads[i])
         total_summits = 0
-        for end in all_ends:
+        for end in distinct_trail_ends:
             if end[0] == 9:
                 total_summits += 1
         ans += total_summits
